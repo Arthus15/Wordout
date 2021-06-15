dbdadb7e2211c8d12c005739440a38b601393ba8
Jairo Blanco <JairoBlanco15@gmail.com>
2021-06-15 22:38:13 +0200

    fix style issue with overflow

--- src/testBuilder.ts
+++ src/testBuilder.ts
@@ -1,6 +1,6 @@
 export class TestBuilder {
     private questionHtml: string =
-        '       <div class="box custom-box">' +
+        '       <div class="box test-box">' +
         '            <h2>{word}</h2>' +
         '            <div class="control">' +
         '                <label class="radio">' +
@@ -16,15 +16,13 @@ export class TestBuilder {
 
 
     private testHTml: string =
-        '<div class="new-test-content-row">' +
-        '   <div class="new-test-content-column">' +
+        '   <div class="test-content-column">' +
         '       <h2 class="timer">00:08:00</h2>' +
         '       {questions}' +
-        '   </div>' +
-        '</div>';
+        '   </div>';
 
     public buildTest(): string {
-        var questions: string[] = ['Prueba1', 'Prueba2', 'Prueba3', 'Prueba4', 'Prueba5', 'Prueba6', 'Prueba7'];
+        var questions: string[] = ['Prueba1', 'Prueba2', 'Prueba3', 'Prueba4', 'Prueba5', 'Prueba6', 'Prueba7', 'Prueba8', 'Prueba9',];
         var result: string[] = [];
         questions.forEach((x) => {
             result.push(this.buildQuestion(x));
