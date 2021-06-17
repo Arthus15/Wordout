593cf7bfda8a24a39d0c336b118f165aabe83382
Jairo Blanco <JairoBlanco15@gmail.com>
2021-06-17 22:10:10 +0200

    Add configuration page style

--- src/database.ts
+++ src/database.ts
@@ -3,17 +3,12 @@ import { open, ISqlite } from 'sqlite';
 import path from 'path';
 
 
-async function dbTestAsync() {
-    console.log('opening sqlite');
-    var db = await open<sqlite3.Database, sqlite3.Statement>({ filename: path.join(__dirname, 'database.db'), driver: sqlite3.Database });
-    await db.exec('CREATE TABLE tbl (col TEXT)');
-    await db.exec('INSERT INTO tbl VALUES ("test")');
-    var row = await db.get('SELECT col FROM tbl WHERE col = ?', 'test');
-    console.log(row);
-}
-
-async function getRow() {
-    var db = await open<sqlite3.Database, sqlite3.Statement>({ filename: path.join(__dirname, 'database.db'), driver: sqlite3.Database });
-    var row = await db.get('SELECT col FROM tbl WHERE col = ?', 'test');
-    console.log(row);
-}
+// async function dbTestAsync() {
+//     console.log('opening sqlite');
+//     var db = await open<sqlite3.Database, sqlite3.Statement>({ filename: path.join(__dirname, 'wordoutDb.db'), driver: sqlite3.Database });
+//     await db.exec('CREATE TABLE words (word TEXT, result INTEGER)');
+//     await db.exec('INSERT INTO words VALUES ("test", 0)');
+//     await db.exec('INSERT INTO words VALUES ("prueba", 1)');
+//     var rows = await db.all('SELECT * FROM words');
+//     console.log(rows);
+// }
