baa4f13366f884cbae2a24496bc9c0f9f1ff7522
Jairo Blanco <JairoBlanco15@gmail.com>
2021-06-21 20:47:34 +0200

    remove word

--- src/wordsService.ts
+++ src/wordsService.ts
@@ -1,13 +1,12 @@
 import { WordoutDb } from './database';
 import { Test } from './models';
 
-var rowHTML = '<tr>' +
+var rowHTML = '<tr id="{word}">' +
     ' <td>{word}</td>' +
     ' <td>{result}</td>' +
+    ' <td> <button class="button is-danger" onClick="deleteWord(\'{word}\')">Eliminar</button></td>' +
     '<tr>';
 var db = new WordoutDb();
-
-
 
 async function initTable(): Promise<void> {
     var words = await db.selectAllAsync<Test[]>('SELECT * FROM words ORDER BY word ASC');
@@ -25,8 +24,18 @@ async function initTable(): Promise<void> {
 
 function buildRow(word: Test): string {
     var row = rowHTML;
-
-    row = row.replace('{word}', word.word);
+    var re = /{word}/gi;
+    row = row.replace(re, word.word);
 
     return row.replace('{result}', word.result ? 'Correcto' : 'Incorrecto');
+}
+
+async function deleteWord(word: string) {
+    var row = document.getElementById(word);
+
+    if (row)
+        row.remove();
+
+    window.resizeBy(0, 0);
+    await db.execAsync(`DELETE FROM words WHERE word = '${word}';`);
 }
